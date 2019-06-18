8f85f6eb5711f525249c411cb0143d9fe826ca13
Franziska B <franziska-katharina.barckmann@stud.uni-bamberg.de>
2019-06-18 11:31:51 +0200

    add test class for EstimationCalculator

---
+++ app/src/test/java/com/example/unmappd/backend/EstimationCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.example.unmappd.backend;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EstimationCalculatorTest {
+
+    @Test
+    public void calculateEstimation() {
+    }
+}
